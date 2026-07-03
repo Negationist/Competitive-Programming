1fffcaa4eff62db55494b7aff0579b8b4f1f82eb
Negationist <jacobeziemeguy@outlook.com>
2026-07-03 17:03:39 -0400

    coded my first treap, not sure if that was worth

--- c++ cp problems/learning/treaps/first treap.cpp
+++ c++ cp problems/learning/treaps/first treap.cpp
@@ -31,7 +31,10 @@ struct Treap_Node{
     ll pri = dist(rng);
     Treap_Node* l = nullptr;
     Treap_Node* r = nullptr;
-    static pair<Treap_Node*, Treap_Node*> split(Treap_Node* x, ll val){
+    Treap_Node(T _key){
+        key = _key;
+    }
+    static pair<Treap_Node*, Treap_Node*> split(Treap_Node* x, T val){
         if(x == nullptr){
             return {nullptr, nullptr};
         }
@@ -48,24 +51,49 @@ struct Treap_Node{
     static Treap_Node* merge(Treap_Node* x, Treap_Node* y){ //all keys of x are smaller than all keys of y
         if(x == nullptr){
             return y;
-        }
+        } 
         if(y == nullptr){
             return x;
         }
         if(x->pri > y->pri){
-            Node* temp = merge(x->r, y);
-            x->r = temp;
+            x->r = merge(x->r, y);
+            return x;
         } else{
-            Node* temp = merge(x, y->l);
-            y->l = temp;
+            y->l = merge(x, y->l);
+            return y;
         }
     }
-    static void insert(Treap_Node* root, Treap_Node* add){
+    static void insert(Treap_Node* &root, T val){ //assumes same thing never gets inserted twice
+        Treap_Node* add = new Treap_Node(val);
         auto [left, right] = split(root, add->key);
-        return merge(merge(left, add), right);
+        root = merge(merge(left, add), right);
     }
-    static bool remove(Treap_Node val){
-
+    static bool remove(Treap_Node* &root, T val){
+        if(root == nullptr){
+            return false;
+        }
+        if(root->key == val){
+            Treap_Node* old = root;
+            root = merge(root->l, root->r);
+            delete old;
+            return true;
+        } else if(root->key < val){
+            return remove(root->r, val);
+        } else{
+            return remove(root->l, val);
+        }
+    }
+    static bool contains(Treap_Node* root, T val){
+        if(root == nullptr){
+            return false;
+        }
+        if(root->key == val){
+            return true;
+        } else if(root->key < val){
+            return contains(root->r, val);
+        } else{
+            return contains(root->l, val);
+        }
     }
 };
 
